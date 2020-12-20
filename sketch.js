093119e5de5139b21bbe52df0e8717e5c10be12d
adwait2007 <74483768+adwait2007@users.noreply.github.com>
2020-12-20 16:09:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var ball;
+var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    database=firebase.database();
+    var ballposition=database.ref('ball/position');
+    ballposition.on('value',readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+} 
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readPosition(data){
+ position=data.val();
+ ball.x=position.x;
+ ball.y=position.y;
+ console.log(position)
+}
+
+function showError(err){
+    console.log(err)
+}
+
+function writePosition(){
+    database.ref('ball/position').set({
+        x:position.x+x,
+        y:position.y+y
+    })
+}
